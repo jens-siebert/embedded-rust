6a714aaf2d11a7a9f61163f4adc2e51c2e0d975b
Jens Siebert <jens.siebert@icloud.com>
2023-11-13 22:33:28 +0100

    Initial implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[allow(unused_imports)]
+use panic_halt as _; // panic handler (link only import)
+use rp_pico::entry;  // rp_pico entry point macro
+
+use rp_pico::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    sio::Sio,
+    watchdog::Watchdog,
+};
+
+use embedded_hal::digital::v2::OutputPin;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = pac::Peripherals::take().unwrap();
+    let core_peripherals = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = Watchdog::new(peripherals.WATCHDOG);
+
+    let clocks = init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
+        peripherals.XOSC,
+        peripherals.CLOCKS,
+        peripherals.PLL_SYS,
+        peripherals.PLL_USB,
+        &mut peripherals.RESETS,
+        &mut watchdog,
+    ).ok().unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core_peripherals.SYST, clocks.system_clock.freq().to_Hz());
+    let sio = Sio::new(peripherals.SIO);
+
+    let pins = rp_pico::Pins::new(
+        peripherals.IO_BANK0,
+        peripherals.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut peripherals.RESETS,
+    );
+
+    let mut led_pin = pins.led.into_push_pull_output();
+
+    loop {
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+    }
 }
